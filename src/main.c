c5891b4a81270c94f668f4b30eec92cacd379713
Zum <seigneurpomidor@gmail.com>
2020-10-31 21:51:26 +0300

    .

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ static int      ft_term_decribe(void)
 
     term_type = getenv("TERM");
     if (!term_type)
-        handle_errors("\nSpecify a terminal type with setenv\n");
+        handle_errors("Specify a terminal type with setenv\n");
     result = tgetent(buf, "TERM");
     if (result < 0)
         handle_errors("Could not access the termcap data base\n");
@@ -33,13 +33,17 @@ static void     fill_catalog(t_select *catalog, int ac, char **av)
         if (!lstat(av[index], &buf))
 			catalog[index].type = buf.st_mode;
 		else
-			handle_errors("lstat failed to recognize type of the file");
+			handle_errors("lstat failed to recognize type of the file\n");
 		catalog[index].last = false;
         index++;
     }
 	catalog[index - 1].last = true;
 }
 
+void	init_tty(t_term *display)
+{
+
+}
 
 int             ft_select(int ac, char **av)
 {
@@ -47,7 +51,10 @@ int             ft_select(int ac, char **av)
     t_term      display;
 
 //signal
+	ft_bzero(&display, sizeof(t_term));
+	ft_bzero(catalog, sizeof(t_select) * (ac - 1));
     fill_catalog(catalog, ac, av);
+	init_tty(&display);
 
     return (0);
 }
